5a40600e0d9a53ec09b061b5a54854cc4c4d4715
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-09-21 10:32:49 +0200

    Added test for Transactions

--- test/test.js
+++ test/test.js
@@ -42,7 +42,7 @@ describe("Node Sybase Bridge", function () {
 
     let connectError;
 
-    beforeEach((done) => {
+    before((done) => {
       sybase = new Sybase(sybaseCredentials);
 
       sybase
@@ -65,6 +65,91 @@ describe("Node Sybase Bridge", function () {
       expect(connectError).to.equal(null);
       expect(sybase.isConnected()).to.equal(true);
       done();
+    });
+
+    describe("Transaction", () => {
+      it("should create a table, insert data and commit the transaction", async function () {
+        expect(connectError).to.equal(null);
+        expect(sybase.isConnected()).to.equal(true);
+
+        await sybase.transaction(async (connection) => {
+          await connection.querySync(
+            "CREATE TABLE test_table (id INT, name VARCHAR(255))"
+          );
+          await connection.querySync(
+            "INSERT INTO test_table (id, name) VALUES (1, 'John')"
+          );
+        });
+
+        const result = await sybase.querySync(
+          "SELECT * FROM test_table WHERE id = 1"
+        );
+
+        expect(result[0][0].name).to.equal("John");
+      });
+
+      it("should rollback the transaction on error", async function () {
+        let errorOccurred = false;
+
+        try {
+          await sybase.transaction(async (connection) => {
+            await connection.querySync(
+              "INSERT INTO test_table (id, name) VALUES (2, 'Jane')"
+            );
+
+            // This should cause an error and rollback the transaction
+            await connection.querySync(
+              "INSERT INTO non_existent_table (id, name) VALUES (1, 'Fail')"
+            );
+          });
+        } catch (err) {
+          expect(err.message.trim()).to.equal(
+            "non_existent_table not found. Specify owner.objectname or use sp_help to check whether the object exists (sp_help may produce lots of output)."
+          );
+
+          errorOccurred = true;
+        }
+
+        expect(errorOccurred).to.equal(true);
+
+        const result = await sybase.querySync(
+          "SELECT * FROM test_table WHERE id = 2"
+        );
+
+        expect(result[0].length).to.equal(0);
+      });
+
+      it('should get error of already created a table named "test_table"', async function () {
+        try {
+          await sybase.transaction(async (connection) => {
+            await connection.querySync(
+              "CREATE TABLE test_table (id INT, name VARCHAR(255))"
+            );
+            await connection.querySync(
+              "INSERT INTO test_table (id, name) VALUES (1, 'John')"
+            );
+          });
+
+          await sybase.querySync("SELECT * FROM test_table WHERE id = 1");
+        } catch (err) {
+          expect(err.message.trim()).to.equal(
+            "There is already an object named 'test_table' in the database."
+          );
+        }
+      });
+
+      it('should clean up by dropping a table named "test_table"', async function () {
+        try {
+          await sybase.querySync("DROP TABLE test_table");
+        } catch (err) {
+          if (
+            !err.message.includes("not found") &&
+            !err.message.includes("does not exist")
+          ) {
+            throw err;
+          }
+        }
+      });
     });
   });
 });
